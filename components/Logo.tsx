7a538596090fe4450518f46cafe14ba3c9e37006
E. Hannah <echannah631@gmail.com>
2026-01-25 16:43:37 -0500

    initial commit, setting up fonts and colors, creating logo, creating hero section

---
+++ components/Logo.tsx
@@ -0,0 +1,7 @@
+export const Logo = () => {
+  return (
+    <p className='font-logo italic text-9xl'>
+      TECH<sup>3</sup>
+    </p>
+  );
+};
---
+++ sections/Hero.tsx
@@ -0,0 +1,12 @@
+import { Logo } from "@/components";
+import React from "react";
+
+export const Hero = () => {
+  return (
+    <section className='w-screen h-screen flex justify-center items-center'>
+      <div>
+        <Logo />
+      </div>
+    </section>
+  );
+};
