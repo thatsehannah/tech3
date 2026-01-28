c35cf8a3c7acb063b229ee6966ca49eacc381dc3
E. Hannah <echannah631@gmail.com>
2026-01-28 00:59:20 -0500

    responsive hero section

--- sections/Hero.tsx
+++ sections/Hero.tsx
@@ -3,11 +3,8 @@ import { Circle } from "lucide-react";
 
 export const Hero = () => {
   return (
-    <section
-      id='hero'
-      className='w-screen h-screen flex justify-center items-center'
-    >
-      <div className='w-1/2 flex flex-col justify-center items-center p-4'>
+    <section id='hero'>
+      <div className='h-screen flex flex-col justify-center items-center -mt-12 md:mt-0 lg:p-8 xl:p-12 border-2 border-yellow-300'>
         <Banner className='gap-2 border-accent3 bg-accent3/20'>
           <Circle
             className='fill-accent3 border-0 stroke-0 animate-pulse'
@@ -17,27 +14,27 @@ export const Hero = () => {
             available to hire
           </p>
         </Banner>
-        <p className='text-lg text-neutral-400 uppercase font-main tracking-widest'>
+        <p className='text-[1rem] xl:text-lg text-neutral-400 uppercase font-main tracking-widest'>
           Welcome To
         </p>
         <Logo />
-        <div className='mt-8'>
-          <p className='font-main text-neutral-400 text-center text-xl'>
+        <div className='mt-4 xl:mt-6 w-3/4 lg:w-1/2'>
+          <p className='font-main text-neutral-400 text-center text-[1rem] lg:text-lg xl:text-xl'>
             Lorem, ipsum dolor sit amet consectetur adipisicing elit.
             Perferendis cupiditate quia mollitia. Consequatur id ipsum, qui
             autem deleniti blanditiis velit vel magnam recusandae doloremque
             maxime neque corporis ab corrupti odit?
           </p>
         </div>
-        <div className='flex justify-center items-center gap-12 p-4 mt-12'>
+        <div className='flex justify-center items-center gap-12 p-4 mt-8 xl:mt-12'>
           <LinkButton
             text='View Projects'
-            className='bg-accent2 hover:shadow-accent2 hover:ring-1 hover:ring-accent1 text-white text-lg font-medium'
+            className='bg-accent2 hover:shadow-accent2 hover:ring-1 hover:ring-accent1 text-white text-[1rem] lg:text-lg font-medium'
             target='projects'
           />
           <LinkButton
             text='Contact Us'
-            className='bg-neutral-800 hover:shadow-white hover:ring-1 hover:ring-white text-white text-lg font-extralight'
+            className='bg-neutral-800 hover:shadow-white hover:ring-1 hover:ring-white text-white text-[1rem] lg:text-lg font-extralight'
             target='contact'
           />
         </div>
